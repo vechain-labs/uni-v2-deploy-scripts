19dd8e9d51c6170ad61863823952106ec29f0210
laalaguer <laalaguer@gmail.com>
2021-03-29 22:51:09 +0800

    deploy

--- deploy_vvet.py
+++ deploy_vvet.py
@@ -1,2 +1,14 @@
 # This script only deploys VVET smart contract.
 # Inputs: network url, json file, deployer wallet
+import sys
+from thor_requests.connect import Connect
+from thor_requests.wallet import Wallet
+
+SCRIPT_NAME = sys.argv[0]
+NETWORK = sys.argv[1] # eg. 'https://solo.veblocks.net'
+PRIVATE_KEY = sys.argv[2]
+VVET_FILE = sys.argv[3]
+
+w = Wallet.fromPrivateKey(bytes.fromhex(PRIVATE_KEY))
+c = Connect(NETWORK)
+
